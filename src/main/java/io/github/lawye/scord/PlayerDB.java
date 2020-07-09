e814172bdd010b864e24f50991c2dad4c9616883
lawye <lawyepc@gmail.com>
2020-07-09 21:15:45 +0800

    hashmap

---
+++ src/main/java/io/github/lawye/scord/PlayerDB.java
@@ -0,0 +1,4 @@
+package io.github.lawye.scord;
+
+public class PlayerDB {
+}
--- src/main/java/io/github/lawye/scord/Scord.java
+++ src/main/java/io/github/lawye/scord/Scord.java
@@ -1,16 +1,29 @@
 package io.github.lawye.scord;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.HashMap;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Scord extends JavaPlugin {
+public final class Scord extends JavaPlugin {
+    public final String URL = "~/Scord.db";
     @Override
     public void onEnable(){
         getLogger().info("Scord start initialize");
     }
-
     @Override
     public void onDisable(){
         getLogger().info("Scord shutdown, backing up");
     }
-
+    public HashMap<String,Integer> loadDB() throws IOException, ClassNotFoundException {
+        try{
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(URL));
+            Object result = ois.readObject();
+            return (HashMap<String,Integer>)result;
+        }catch(Exception err){
+            err.printStackTrace();
+        }
+    }
 }
